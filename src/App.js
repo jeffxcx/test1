f964e57d35cc092adf9dd1cede734cd52fff0c82
Jeff <118344787+jeffxcx@users.noreply.github.com>
2024-07-10 18:43:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { API } from 'aws-amplify';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const handleSendMessage = async () => {
+    const response = await API.post('ChatAPI', '/send', {
+      body: { message },
+    });
+    setMessages([...messages, response]);
+    setMessage('');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +31,32 @@ function App() {
           Learn React
         </a>
       </header>
+      <Chat
+        message={message}
+        setMessage={setMessage}
+        messages={messages}
+        setMessages={setMessages}
+        handleSendMessage={handleSendMessage}
+      />
+    </div>
+  );
+}
+
+function Chat({ message, setMessage, messages, setMessages, handleSendMessage }) {
+  return (
+    <div>
+      <h2>Chat Room</h2>
+      <div>
+        {messages.map((msg, index) => (
+          <div key={index}>{msg.message}</div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={handleSendMessage}>Send</button>
     </div>
   );
 }
